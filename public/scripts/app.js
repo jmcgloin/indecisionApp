765e72c65091e71b0cb68c1ee0c6d23941fd5c8e
Jason McGloin <jason.mcgloin@gmail.com>
2019-05-10 13:50:02 -0400

    working with multiline jsx  and objects

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,58 @@
-"use strict";
+'use strict';
 
 console.log("app.js is running");
 
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		'Indecision App'
+	),
+	React.createElement(
+		'p',
+		null,
+		'This is some info'
+	),
+	React.createElement(
+		'p',
+		null,
+		'It seems to like parenthesis!'
+	)
+);
+
+var user = {
+	name: 'Jason',
+	age: 42,
+	location: 'Akron, OH'
+	// var username = 'Jason';
+	// var userage = 43;
+	// var userlocation = 'Akron, OH';
+
+};var template2 = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		'Name: ',
+		user.name + '!'
+	),
+	React.createElement(
+		'p',
+		null,
+		'Age: ',
+		user.age
+	),
+	React.createElement(
+		'p',
+		null,
+		'Location: ',
+		user.location
+	)
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
--- source/app.js
+++ source/app.js
@@ -1,7 +1,30 @@
 console.log("app.js is running");
 
-var template = <h1>Indecision App</h1>;
+var template = (
+	<div>
+		<h1>Indecision App</h1>
+		<p>This is some info</p>
+		<p>It seems to like parenthesis!</p>
+	</div>
+);
+
+var user = {
+	name :'Jason',
+	age: 42,
+	location: 'Akron, OH'
+}
+// var username = 'Jason';
+// var userage = 43;
+// var userlocation = 'Akron, OH';
+
+var template2 = (
+	<div>
+		<h1>Name: {user.name + '!'}</h1>
+		<p>Age: {user.age}</p>
+		<p>Location: {user.location}</p>
+	</div>
+);
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(template2, appRoot)
