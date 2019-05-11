ea790102627849f218fc927712f6e864fa725e27
Jason McGloin <jason.mcgloin@gmail.com>
2019-05-11 18:01:52 -0400

    adding event handlers

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,82 +3,122 @@
 console.log("app.js is running");
 
 var app = {
-	title: "Indecision App"
-	// subtitle: "I can't decide!",
-	// options: ["One", "Two"]
+	title: "Indecision App",
+	subtitle: "I can't decide!",
+	ideas: []
 };
 
-var template = React.createElement(
-	"div",
-	null,
-	React.createElement(
-		"h1",
-		null,
-		app.title
-	),
-	app.subtitle && React.createElement(
-		"p",
-		null,
-		app.subtitle
-	),
-	React.createElement(
-		"p",
-		null,
-		app.options && app.options.length > 0 ? "Here are your options" : "There are no options"
-	),
-	React.createElement(
-		"ol",
-		null,
-		React.createElement(
-			"li",
-			null,
-			app.options && app.options[0]
-		),
-		React.createElement(
-			"li",
-			null,
-			app.options && app.options[1]
-		)
-	)
-);
-
-var getLocation = function getLocation(location) {
-	if (location) return React.createElement(
-		"p",
-		null,
-		"Location: ",
-		location
-	);
+var onFormSubmit = function onFormSubmit(e) {
+	e.preventDefault();
+	var idea = e.target.elements.idea.value;
+	idea.trim() && app.ideas.push(idea);
+	e.target.elements.idea.value = "";
+	renderIndApp();
 };
 
-var user = {
-	name: 'Jason McGloin',
-	age: 2,
-	location: 'Akron, OH'
-	// var username = 'Jason';
-	// var userage = 43;
-	// var userlocation = 'Akron, OH';
-
-};var template2 = React.createElement(
-	"div",
-	null,
-	React.createElement(
-		"h1",
-		null,
-		"Name: ",
-		user.name ? user.name : "Anonymous"
-	),
-	user.age && user.age >= 18 && React.createElement(
-		"p",
-		null,
-		"Age: ",
-		user.age
-	),
-	getLocation(user.location)
-);
+var onRemoveAll = function onRemoveAll() {
+	app.ideas.length = 0;
+	renderIndApp();
+};
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var renderIndApp = function renderIndApp() {
+	var template = React.createElement(
+		"div",
+		null,
+		React.createElement(
+			"h1",
+			null,
+			app.title
+		),
+		app.subtitle && React.createElement(
+			"p",
+			null,
+			app.subtitle
+		),
+		React.createElement(
+			"p",
+			null,
+			app.ideas && app.ideas.length > 0 ? "Here are your ideas" : "There are no ideas"
+		),
+		React.createElement(
+			"p",
+			null,
+			app.ideas.length
+		),
+		React.createElement(
+			"button",
+			{ onClick: onRemoveAll },
+			"Remove All"
+		),
+		React.createElement(
+			"ol",
+			null,
+			React.createElement(
+				"li",
+				null,
+				"Idea 1"
+			),
+			React.createElement(
+				"li",
+				null,
+				"Idea 2"
+			)
+		),
+		React.createElement(
+			"form",
+			{ onSubmit: onFormSubmit },
+			React.createElement(
+				"label",
+				{ htmlFor: "idea", name: "idea" },
+				"Enter your idea here: "
+			),
+			React.createElement("input", { type: "text", name: "idea", id: "idea" }),
+			React.createElement(
+				"button",
+				null,
+				"Add Idea"
+			)
+		)
+	);
+
+	ReactDOM.render(template, appRoot);
+};
+
+renderIndApp();
+
+// let count = 0;
+// const increment = () => {
+// 	count++;
+// 	renderCounterApp();
+// }
+// const decrement = () => {
+// 	count--;
+// 	renderCounterApp();
+// }
+// const reset = () => {
+// 	count = 0;
+// 	renderCounterApp();
+// }
+
+// const appRoot = document.getElementById('app');
+
+// const renderCounterApp = () => {
+// 	const  templateTwo = (
+// 		<div>
+// 			<h1>
+// 				Count: {count}
+// 			</h1>
+// 			<button onClick={increment}>+1</button>
+// 			<button onClick={decrement}>-1</button>
+// 			<button onClick={reset}>Reset</button>
+// 		</div>
+// 	);
+
+// 	ReactDOM.render(templateTwo, appRoot);
+// }
+
+// renderCounterApp();
 
 // babel source/app.js --out-file=public/scripts/app.js --presets=env,react --watch
--- source/app.js
+++ source/app.js
@@ -3,45 +3,82 @@ console.log("app.js is running");
 
 const app = {
 	title: "Indecision App",
-	// subtitle: "I can't decide!",
-	// options: ["One", "Two"]
+	subtitle: "I can't decide!",
+	ideas: []
 }
 
-const template = (
-	<div>
-		<h1>{app.title}</h1>
-		{app.subtitle && <p>{app.subtitle}</p>}
-		<p>{(app.options && app.options.length > 0) ? "Here are your options" : "There are no options"}</p>
-		<ol>
-			<li>{app.options && app.options[0]}</li>
-			<li>{app.options && app.options[1]}</li>
-		</ol>
-	</div>
-);
-
-const getLocation = (location) => {
-	if (location) return <p>Location: {location}</p>;
-};
-
-const user = {
-	name: 'Jason McGloin',
-	age: 2,
-	location: 'Akron, OH'
+const onFormSubmit = (e) => {
+	e.preventDefault();
+	const idea = e.target.elements.idea.value;
+	idea.trim() && app.ideas.push(idea);
+	e.target.elements.idea.value = "";
+	renderIndApp();
 }
-// var username = 'Jason';
-// var userage = 43;
-// var userlocation = 'Akron, OH';
 
-const template2 = (
-	<div>
-		<h1>Name: {user.name ? user.name : "Anonymous"}</h1>
-		{(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-		{getLocation(user.location)}
-	</div>
-);
+const onRemoveAll = () => {
+	app.ideas.length = 0;
+	renderIndApp();
+}
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot)
+const renderIndApp = () =>  {
+	const template = (
+		<div>
+			<h1>{app.title}</h1>
+			{app.subtitle && <p>{app.subtitle}</p>}
+			<p>{(app.ideas && app.ideas.length > 0) ? "Here are your ideas" : "There are no ideas"}</p>
+			<p>{app.ideas.length}</p>
+			<button onClick={onRemoveAll}>Remove All</button>
+			<ol>
+				<li>Idea 1</li>
+				<li>Idea 2</li>
+			</ol>
+			<form onSubmit={onFormSubmit}>
+				<label htmlFor="idea" name="idea">Enter your idea here: </label>
+				<input type="text" name="idea" id="idea" />
+				<button>Add Idea</button>
+			</form>
+		</div>
+	);
+
+	ReactDOM.render(template, appRoot);
+
+};
+
+renderIndApp();
+
+// let count = 0;
+// const increment = () => {
+// 	count++;
+// 	renderCounterApp();
+// }
+// const decrement = () => {
+// 	count--;
+// 	renderCounterApp();
+// }
+// const reset = () => {
+// 	count = 0;
+// 	renderCounterApp();
+// }
+
+// const appRoot = document.getElementById('app');
+
+// const renderCounterApp = () => {
+// 	const  templateTwo = (
+// 		<div>
+// 			<h1>
+// 				Count: {count}
+// 			</h1>
+// 			<button onClick={increment}>+1</button>
+// 			<button onClick={decrement}>-1</button>
+// 			<button onClick={reset}>Reset</button>
+// 		</div>
+// 	);
+
+// 	ReactDOM.render(templateTwo, appRoot);
+// }
+
+// renderCounterApp();
 
 // babel source/app.js --out-file=public/scripts/app.js --presets=env,react --watch
